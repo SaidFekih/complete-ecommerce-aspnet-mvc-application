310834fe48bf71950fb76888652d0ba5fdda9012
SaidFekih <saidgwx@gmail.com>
2023-03-14 20:02:09 -0400

    First commit

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,10 @@
+using eTickets.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//DbContex configuration
+builder.Services.AddDbContext<AppDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
